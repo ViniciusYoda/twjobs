feeb00254d7d90c0992f49b3857cd9c417c92e0b
Vinícius Yoda <vyoda4604@gmail.com>
2025-05-26 19:11:07 -0300

    end

--- skills/urls.py
+++ skills/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from rest_framework import routers
 
-from .views import SkillListView, SkillDetailView
+from .views import SkillViewSet
 
 app_name = "skills"
+router = routers.SimpleRouter()
+router.register(r'', SkillViewSet, basename="skill")
+urlpatterns = router.urls
 
-urlpatterns = [
-    path("", SkillListView.as_view(), name="skill-list"),
-    path("<int:pk>/", SkillDetailView.as_view(), name="skill-detail"),
-]
